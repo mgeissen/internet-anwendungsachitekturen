a02b3961632a376ad02f3bc750afad3b3c097cb3
mgeissen <matt.geis97@gmail.com>
2018-08-06 12:46:51 +0200

    add serualizazion exampke

---
+++ serialization/src/main/java/de/nordakademie/iaa/serialization/Book.java
@@ -0,0 +1,88 @@
+package de.nordakademie.iaa.serialization;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Book implements Serializable {
+
+    private String name;
+    private String author;
+    private LocalDate datePublished;
+    private String isbn;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getDatePublished() {
+        return datePublished;
+    }
+
+    public void setDatePublished(LocalDate datePublished) {
+        this.datePublished = datePublished;
+    }
+
+
+    public static final class Builder {
+        private String name;
+        private String author;
+        private LocalDate datePublished;
+        private String isbn;
+
+        private Builder() {
+        }
+
+        public static Builder aBook() {
+            return new Builder();
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withAuthor(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder withDatePublished(LocalDate datePublished) {
+            this.datePublished = datePublished;
+            return this;
+        }
+
+        public Builder withIsbn(String isbn) {
+            this.isbn = isbn;
+            return this;
+        }
+
+        public Book build() {
+            Book book = new Book();
+            book.setName(name);
+            book.setAuthor(author);
+            book.setDatePublished(datePublished);
+            book.setIsbn(isbn);
+            return book;
+        }
+    }
+}
---
+++ serialization/src/main/java/de/nordakademie/iaa/serialization/SerialisationExample.java
@@ -0,0 +1,29 @@
+package de.nordakademie.iaa.serialization;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+
+public class SerialisationExample {
+
+    public static void main(String[] args) {
+        // create a book
+        Book book = Book.Builder.aBook()
+                .withAuthor("Matze")
+                .withDatePublished(LocalDate.of(1997, 7, 3))
+                .withIsbn("123-123-123-123")
+                .withName("Nicer Dicer")
+                .build();
+
+        // save the book
+        try(ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("book.object"))) {
+            outputStream.writeObject(book);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
